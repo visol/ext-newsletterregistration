b0fb080ec3cb5140463fe527a5d56ae10a6689da
Daniel Huf <daniel.huf@visol.ch>
2022-10-07 16:19:26 +0200

    [TASK] Remove unused code and simplify the random string

--- Classes/Utility/Algorithms.php
+++ Classes/Utility/Algorithms.php
@@ -2,7 +2,6 @@
 
 namespace Visol\Newsletterregistration\Utility;
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\SingletonInterface;
 /*                                                                        *
  * This script belongs to the TYPO3 Flow framework.                       *
@@ -13,7 +12,6 @@ use TYPO3\CMS\Core\SingletonInterface;
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
-require_once(ExtensionManagementUtility::extPath('newsletterregistration') . 'Resources/PHP/iSecurity/Security_Randomizer.php');
 
 /**
  * A utility class for various algorithms.
@@ -21,53 +19,22 @@ require_once(ExtensionManagementUtility::extPath('newsletterregistration') . 'Re
  */
 class Algorithms implements SingletonInterface
 {
-
-    /**
-     * Generates a universally unique identifier (UUID) according to RFC 4122.
-     * The algorithm used here, might not be completely random.
-     *
-     * @return string The universally unique id
-     * @todo check for randomness, optionally generate type 1 and type 5 UUIDs, use php5-uuid extension if available
-     */
-    static public function generateUUID()
-    {
-        return strtolower(\Security_Randomizer::getRandomGUID());
-    }
-
-    /**
-     * Returns a string of random bytes.
-     *
-     * @param integer $count Number of bytes to generate
-     * @return string Random bytes
-     */
-    static public function generateRandomBytes($count)
-    {
-        return \Security_Randomizer::getRandomBytes($count);
-    }
-
-    /**
-     * Returns a random token in hex format.
-     *
-     * @param integer $count Token length
-     * @return string A random token
-     */
-    static public function generateRandomToken($count)
-    {
-        return \Security_Randomizer::getRandomToken($count);
-    }
-
     /**
      * Returns a random string with alpha-numeric characters.
      *
      * @param integer $count Number of characters to generate
      * @param string $characters Allowed characters, defaults to alpha-numeric (a-zA-Z0-9)
      * @return string A random string
+     * @throws \Exception
      */
-    static public function generateRandomString(
-        $count,
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    ) {
-        return \Security_Randomizer::getRandomString($count, $characters);
-    }
+    public static function generateRandomString(int $count, string $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'): string
+    {
+        $characterCount = strlen($characters);
+        $string = '';
+        for ($i = 0; $i < $count; $i++) {
+            $string .= $characters[random_int(0, ($characterCount - 1))];
+        }
 
+        return $string;
+    }
 }
