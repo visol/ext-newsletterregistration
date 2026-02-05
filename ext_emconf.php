bae2b477d1c9a797463f95fdca857d7b64d3bc28
Andy Meier <andy.meier@visol.ch>
2026-02-05 18:09:03 +0100

    chore: bump verson number to 4.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,5 +6,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Lorenz Ulrich',
     'author_email' => 'lorenz.ulrich@visol.ch',
     'author_company' => 'visol digitale Dienstleistungen AG',
-    'version' => '4.0.2',
+    'version' => '4.0.3',
 ];
